014a70a8b5acb6f90533c510a2cb9d6e5fba53c5
jinsung kim <godidtntksqn@gmail.com>
2023-09-08 09:54:23 +0900

    color-change

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let randomNumber = 0;
+let randomColorNumber = 0;
 const userInput = document.querySelector("input");
 const startBtn = document.getElementById("start-btn");
 const resetBtn = document.getElementById("reset-btn");
@@ -6,6 +7,23 @@ const resultArea = document.getElementById("result-area");
 const chanceArea = document.getElementById("chance-area");
 let chances = 5;
 let historyArray = [];
+const colors = [
+  "black",
+  "#1f77b4",
+  "#ff7f0e",
+  "#2ca02c",
+  "#d62728",
+  "#9467bd",
+  "#8c564b",
+  "#e377c2",
+  "#7f7f7f",
+  "#bcbd22",
+  "#17becf",
+];
+
+function randomColorMaker() {
+  randomColorNumber = Math.floor(Math.random() * colors.length);
+}
 
 function randomMaker() {
   randomNumber = Math.floor(Math.random() * 100) + 1;
@@ -14,6 +32,8 @@ function randomMaker() {
 randomMaker();
 
 function gameStart() {
+  chanceArea.classList.add("blink");
+
   let userInputValue = userInput.value;
 
   if (historyArray.includes(userInputValue)) {
@@ -30,6 +50,8 @@ function gameStart() {
 
   chances--;
   chanceArea.innerText = `당신의 남은 목숨 : ${chances} 개`;
+  randomColorMaker();
+  chanceArea.style.color = colors[randomColorNumber];
 
   if (userInputValue < randomNumber) {
     resultArea.innerText = "숫자좀 더 써봐..";
@@ -50,6 +72,7 @@ function gameStart() {
 function gameReset() {
   randomMaker();
   chances = 5;
+  chanceArea.style.color = "black";
   userInput.value = "";
   historyArray = [];
   chanceArea.innerText = "남은 목숨 : 5개";
